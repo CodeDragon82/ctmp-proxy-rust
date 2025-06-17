f29a348c11bb392cd62d64558671cec7ae68c6f9
CodeDragon82 <michaelrichar280@outlook.com>
2025-06-17 12:16:59 +0100

    Read data from the source client

--- ctmp_proxy/src/main.rs
+++ ctmp_proxy/src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 use std::process;
 
 const LOCALHOST: &str = "127.0.0.1";
@@ -31,19 +32,40 @@ fn create_listener(port: &str) -> TcpListener {
     };
 }
 
+fn read_from_source(source: &mut TcpStream, buffer: &mut [u8]) {
+    match source.read(buffer) {
+        Ok(0) => {},
+        Ok(byte_count) => println!("{} bytes read from source", byte_count),
+        Err(e) => eprintln!("Failed to read from source: {}", e)
+    }
+}
+
 fn main() {
     let source_socket: TcpListener = create_listener(SOURCE_PORT);
     let destination_socket: TcpListener = create_listener(DESTINATION_PORT);
 
+    let mut source_client: Option<TcpStream> = None;
+
+    let mut buffer: [u8; 70000] = [0; 70000];
+
     loop {
         match source_socket.accept() {
-            Ok((stream, socket_address)) => println!("New source connection: {}", socket_address.port()),
+            Ok((stream, socket_address)) => {
+                println!("New source connection: {}", socket_address.port());
+                source_client = Some(stream);
+            },
             Err(e) => {}
         }
 
         match destination_socket.accept() {
             Ok((stream, socket_addres)) => println!("New destination connection: {}", socket_addres.port()),
             Err(e) => {}
+        }
+
+        // Attempt to read data from source client if connected.
+        match source_client.as_mut() {
+            Some(source) => read_from_source(source, &mut buffer),
+            None => {}
         }
     }
 }
